a6f176b110a60e9af3f57d27b913d94daf19cfd3
Pulimet <korolev2009@gmail.com>
2019-04-27 15:51:40 +0300

    Add Coroutines

--- app/src/main/java/net/alexandroid/teamco/App.kt
+++ app/src/main/java/net/alexandroid/teamco/App.kt
@@ -1,19 +1,33 @@
 package net.alexandroid.teamco
 
 import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import net.alexandroid.shpref.ShPref
 import net.alexandroid.teamco.di.KoinLogs
 import net.alexandroid.teamco.di.appModule
 import net.alexandroid.utils.mylog.MyLog
 import org.koin.android.ext.android.startKoin
+import kotlin.coroutines.CoroutineContext
 
-class App : Application() {
+class App : Application(), CoroutineScope {
+
+    override val coroutineContext: CoroutineContext = Dispatchers.IO
+
     override fun onCreate() {
         super.onCreate()
-
-        MyLog.init(this, "QAZ", BuildConfig.DEBUG)
-        ShPref.init(this, ShPref.APPLY)
-        startKoin(applicationContext, listOf(appModule), logger = KoinLogs())
-
+        setLogger()
+        launch {
+            ShPref.init(applicationContext, ShPref.APPLY)
+            startKoin(applicationContext, listOf(appModule), logger = KoinLogs())
+        }
     }
+
+    private fun setLogger() {
+        MyLog.init(applicationContext, "QAZ", BuildConfig.DEBUG)
+        MyLog.setThreadIdVisibility(true)
+        MyLog.e("============ Application Created ==============")
+    }
+
 }
