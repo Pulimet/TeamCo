5d97a3edbc7b8a75dab4651bf39cd8147e3dc157
Pulimet <korolev2009@gmail.com>
2019-04-27 15:24:01 +0300

    Add MyLog and ShPref libs

--- app/src/main/java/net/alexandroid/teamco/App.kt
+++ app/src/main/java/net/alexandroid/teamco/App.kt
@@ -1,10 +1,15 @@
 package net.alexandroid.teamco
 
 import android.app.Application
+import net.alexandroid.shpref.ShPref
+import net.alexandroid.utils.mylog.MyLog
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        MyLog.init(this, "QAZ", BuildConfig.DEBUG)
+        ShPref.init(this, ShPref.APPLY)
 
     }
 }
